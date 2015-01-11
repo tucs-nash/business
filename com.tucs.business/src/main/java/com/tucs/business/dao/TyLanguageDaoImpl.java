ac4ca4f183f92b7132da0838c9b8f46491524c4c
Jessica Braga <dimbraga@gmail.com>
2015-01-11 12:57:33 +0000

    Correction of the TyLanguageDaoImpl with the anotation @Repository

--- com.tucs.business/src/main/java/com/tucs/business/dao/TyLanguageDaoImpl.java
+++ com.tucs.business/src/main/java/com/tucs/business/dao/TyLanguageDaoImpl.java
@@ -1,8 +1,11 @@
 package com.tucs.business.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.tucs.business.dao.interfaces.TyLanguageDao;
 import com.tucs.core.model.entity.TyLanguage;
 
+@Repository
 public class TyLanguageDaoImpl  extends AbstractBaseDao<TyLanguage> implements TyLanguageDao {
 
 	public TyLanguageDaoImpl() {
